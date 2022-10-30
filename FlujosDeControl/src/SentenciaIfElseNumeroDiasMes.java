b9b09bce9f45aac9b87daddd79e5f4a55a4e98ba
ErikSon1305 <114564612+ErikSon1305@users.noreply.github.com>
2022-10-30 12:07:46 +0100

    1

--- FlujosDeControl/src/SentenciaIfElseNumeroDiasMes.java
+++ FlujosDeControl/src/SentenciaIfElseNumeroDiasMes.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class SentenciaIfElseNumeroDiasMes {
     public static void main(String[] args) {
 
--- FlujosDeControl/src/SentenciasSwitchCase.java
+++ FlujosDeControl/src/SentenciasSwitchCase.java
@@ -1,5 +1,58 @@
+import java.util.Scanner;
+
 public class SentenciasSwitchCase {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int mes = 0;
+        System.out.println("Ingrese el numero de mes 1-12: ");
+        mes = sc.nextInt();
+
+        String nombreMes = null;
+
+        switch (mes){
+            case 1:
+                nombreMes = "Enero";
+                break;
+            case 2:
+                nombreMes = "Febrero";
+                break;
+            case 3:
+                nombreMes = "Marzo";
+                break;
+            case 4:
+                nombreMes = "Abril";
+                break;
+            case 5:
+                nombreMes = "Mayo";
+                break;
+            case 6:
+                nombreMes = "Junio";
+                break;
+            case 7:
+                nombreMes = "Julio";
+                break;
+            case 8:
+                nombreMes = "Agosto";
+                break;
+            case 9:
+                nombreMes = "Septiembre";
+                break;
+            case 10:
+                nombreMes = "Octubre";
+                break;
+            case 11:
+                nombreMes = "Noviembre";
+                break;
+            case 12:
+                nombreMes = "Diciembre";
+                break;
+            default:
+                nombreMes = "Indefinido";
+        }
+
+        System.out.println("nombreMes = " + nombreMes);
+
 
         int num = 1;
         switch (num){
@@ -26,6 +79,11 @@ public class SentenciasSwitchCase {
             case "Erik":
                 System.out.println("Hola " + nombre + " , bienvenido!");
                 break;
+            case "pepe":
+                System.out.println("Hola Pepe, bienvenido!");
+                break;
+            default:
+                System.out.println("Usuario desconocido!");
         }
     }
 }
