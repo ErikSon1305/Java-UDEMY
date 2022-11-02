704d1fa3648ff8ad7b858e07196ab168bd7866d1
ErikSon1305 <114564612+ErikSon1305@users.noreply.github.com>
2022-11-02 09:12:16 +0100

    1

--- FlujosDeControl/src/SentenciaForEach.java
+++ FlujosDeControl/src/SentenciaForEach.java
@@ -1,17 +1,2 @@
 public class SentenciaForEach {
-    public static void main(String[] args) {
-
-        int [] numeros = {1, 3, 5 , 7, 9 ,11, 13, 14};
-
-        for (int num: numeros){
-            System.out.println("num = " + num);
-        }
-
-        String [] nombres = {"Erik", "Sven", "Gemma", "Francisco"};
-
-        for (String nombre : nombres){
-            System.out.println("nombre = " + nombre);
-        }
-
-    }
 }
